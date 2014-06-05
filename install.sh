91a17ed748ed07665e7e8f582cadd1638d4c127c
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-06-06 02:12:08 +0900

    ファイルのリネームとcaskのインストール

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 relative_dir=`dirname $0`
 dotfiles_dir=`(cd $relative_dir; pwd)`
 echo $dotfiles_dir
-for file in .zshrc .vimrc .tmux.conf .gitconfig
+
+for file in zshrc vimrc tmux.conf gitconfig
 do
   src=$dotfiles_dir/$file
-  ln -fs $src $HOME
+  ln -fs $src $HOME/.$file
 done
+
+if [ ! -d "${HOME}/.cask" ]; then
+    curl -fsSkL https://raw.github.com/cask/cask/master/go | python 
+fi
