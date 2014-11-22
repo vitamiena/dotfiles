f339da6bbb6c1bdc3a60ea6a88fcd4cf064ab7b1
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-11-22 18:42:54 +0900

    IPの取得をコマンドの戻り値で判断するように修正

--- zsh.d/ipaddr_l.sh
+++ zsh.d/ipaddr_l.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ `/sbin/ifconfig | grep eth1`]; then
+/sbin/ifconfig | grep eth1 > /dev/null
+
+if [ $? -eq 0 ]; then
     /sbin/ifconfig eth1 | grep inet | grep -v inet6 | cut -f 2 -d':' | cut -d' ' -f 1
 else
     /sbin/ifconfig eth0 | grep inet | grep -v inet6 | cut -f 2 -d':' | cut -d' ' -f 1
